54e2863e91912382276dc4a7d88081ebb355eb95
Son Vu <son.vu@beu.ai>
2019-09-20 09:38:03 +0200

    Example on how to reload text data with item id

---
+++ README.md
@@ -0,0 +1,2 @@
+# How to reload text data
+
---
+++ reload_df_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+train_text_data_df = pd.read_csv("./02_train_text.csv", quotechar='"', sep=",")
+test_text_data_df = pd.read_csv("./04_test_text.csv", quotechar='"', sep=",")
+
